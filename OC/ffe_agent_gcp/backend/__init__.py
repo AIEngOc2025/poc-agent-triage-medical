3d41e04c147bfa08836813967330d8697361c4c1
phenixMaster <christophempaga@gmail.com>
2026-05-25 20:03:12 +0200

    chore: remove ffe_agent_gcp directory from git tracking (15K files cleanup)

--- OC/ffe_agent_gcp/backend/__init__.py
+++
@@ -1,3 +0,0 @@
-from backend.ffe_chess_agent import agent
-
-from . import agent
--- OC/ffe_agent_gcp/backend/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .milvus_tools import query_wikichess
-from .stockfish_tools import evaluate_position
-from .youtube_tools import search_chess_videos
